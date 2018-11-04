9a95816b56e85ad28b05bb0c007cf8e73ddbc442
makepluscode <39910774+makepluscode@users.noreply.github.com>
2018-11-04 20:47:05 +0900

    Update main.cpp

--- 008-Webserver-UDP/main.cpp
+++ 008-Webserver-UDP/main.cpp
@@ -1,16 +1,11 @@
-#include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 WiFiUDP udp;
 
-const char *SSID = "CherryBongBong-Home";
-const char *PASS = "hahaha123!";
-
-unsigned int localUdpPort = 4210;  // local port to listen on
-char incomingPacket[255];  // buffer for incoming packets
-char  replyPacket[] = "Hi there! Got the message :-)";  // a reply string to send back
+// Make sure to add your own information
+const char *SSID = "****";
+const char *PASS = "****";
 
 void setup()
 { 
@@ -23,33 +18,27 @@ void setup()
     delay(100);
     Serial.print(".");
   }
-  Serial.println("Connected!");
-
-  Serial.println("[NETWORK]");
+  Serial.print("Connected! ");
   Serial.println(WiFi.localIP());
-  Serial.println(WiFi.macAddress());
-  Serial.println("");
 
   udp.begin(1004);
 }
 
 void loop()
 {
-  int packetSize = udp.parsePacket();
-  if (packetSize)
-  {
-    // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
-    int len = udp.read(incomingPacket, 255);
-    if (len > 0)
-    {
-      incomingPacket[len] = 0;
-    }
-    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+  char buffer[255];
+  int size = udp.parsePacket();
 
-    // send back a reply, to the IP address and port we got the packet from
+  if (size)
+  {
+    int len;
+    memset(buffer, 0x0, sizeof(buffer));
+
+    len = udp.read(buffer, sizeof(buffer));
+    Serial.printf("Received Packet : %s\n", buffer);
+
     udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write(replyPacket);
+    udp.write("Welcomm to Wemos");
     udp.endPacket();
   }
 }
